4650a12aa17ee23ea3b95954a30bd0b4399c5f1b
Sam Crooks <samcrooks@Sams-MacBook-Pro.local>
2019-07-16 16:32:28 -0600

    David's example of how to pass button info to event listener

--- js/map_script.js
+++ js/map_script.js
@@ -1,5 +1,3 @@
-console.log(coordinates[0][0]);
-
 var map = L.map('map').setView([35.084385, -106.650421], 11.1);
 		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
