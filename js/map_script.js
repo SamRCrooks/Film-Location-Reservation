addbd43cb9a53c062a08c84d20662a9d864610d4
Sam Crooks <samcrooks@Sams-MacBook-Pro.local>
2019-07-16 09:42:06 -0600

    Icons added to map

--- js/map_script.js
+++ js/map_script.js
@@ -5,15 +5,33 @@ var map = L.map('map').setView([35.084385, -106.650421], 11.1);
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+
+var greenIcon = L.icon({
+    iconUrl: 'icons/camera_green.png',
+
+    iconSize:     [50, 50], // size of the icon
+    iconAnchor:   [28, 94], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+var redIcon = L.icon({
+    iconUrl: 'icons/camera_red.png',
+
+    iconSize:     [50, 50], // size of the icon
+    iconAnchor:   [28, 94], // point of the icon which will correspond to marker's location
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+
 var markers = [];
 
-var testMarker = L.marker([35.047711, -106.605247]).addTo(map)
-    .bindPopup('BREAKING BAD SHOT HERE')
-    .openPopup();
+//var testMarker = L.marker([35.047711, -106.605247], {icon: greenIcon}).addTo(map)
+//    .bindPopup('BREAKING BAD SHOT HERE')
+//    .openPopup();
 
 for (i = 0; i <coordinates.length; i++){
 	console.log(coordinates[i][0], coordinates[i][1]);
-	L.marker([coordinates[i][0], coordinates[i][1]]).addTo(map)
+	L.marker([coordinates[i][0], coordinates[i][1]], {icon: greenIcon}).addTo(map)
     .bindPopup(sites[i])
     .openPopup();
 }
